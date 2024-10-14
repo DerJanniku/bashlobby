76dbcfc4d1a95bd2ec99ad1545fa75fecf0788b7
BuildTools <unconfigured@null.spigotmc.org>
2024-10-14 18:38:40 +0200

    secomd commit

--- src/main/java/org/derjannik/bashlobby/commands/ClanCommand.java
+++ src/main/java/org/derjannik/bashlobby/commands/ClanCommand.java
@@ -4,10 +4,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 
 public class ClanCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("clan")) {
             if (args.length < 1) {
                 sender.sendMessage(ChatColor.RED + "Usage: /clan <create|disband|list> [clanName]");
